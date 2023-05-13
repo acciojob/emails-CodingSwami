7f72e3583d019d114f657552d293903613859d82
Gaurav Goswami <gauravg3070@gmail.com>
2023-05-13 22:03:58 +0530

    push 3

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,12 +1,11 @@
 package com.driver;
-import java.util.*;
 
 public class Email {
 
     private String emailId;
     private String password;
 
-    public Email(String emailId) {
+    public Email(String emailId){
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -19,45 +18,56 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword) {
+    public void changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        if (!oldPassword.equals(password))
-            return;
 
-        if (newPassword.length() < 8)
-            return;
+        if(oldPassword.equals(password)){
+            if(isValid(newPassword)){
+                System.out.println("Password changed successfully!");
+                this.password = newPassword;
+            }
+            else{
+                System.out.println("The new Password is not valid!");
+            }
+        }
+        else{
+            System.out.println("The given password does not match current password!");
+        }
+    }
 
-        char[] arr = newPassword.toCharArray();
-        boolean isUpperCasePresent = false;
-        boolean islowercasePresent = false;
-        boolean isDigitPresent = false;
-        boolean isSpecialCharacterPresent = false;
+    private Boolean isValid(String password){
+        Boolean capitalLetter = false;
+        Boolean smallLetter = false;
+        Boolean digit = false;
+        Boolean specialCharacter = false;
 
-        int i = 0;
-
-        while (i < arr.length && (!isUpperCasePresent || !islowercasePresent || !isDigitPresent || !isSpecialCharacterPresent)) {
-
-            if (Character.isUpperCase(arr[i]))
-                isUpperCasePresent = true;
-            else if (Character.isLowerCase(arr[i]))
-                islowercasePresent = true;
-            else if (Character.isDigit(arr[i]))
-                isDigitPresent = true;
-            else
-                isSpecialCharacterPresent = true;
-
-            if (isUpperCasePresent && islowercasePresent && isDigitPresent && isSpecialCharacterPresent)
-                break;
-
-            i++;
+        // checking length of password
+        if(password.length() < 8){
+            return false;
         }
 
-        if (isUpperCasePresent && islowercasePresent && isDigitPresent && isSpecialCharacterPresent)
-            password = newPassword;
+        for(int i=0;i<password.length(); i++){
+            char ch = password.charAt(i);
+            if(ch >= 'A' && ch <= 'Z'){
+                capitalLetter = true;
+            }
+            else if(ch >= 'a' && ch <= 'z' ){
+                smallLetter = true;
+            }
+            else if(ch >= '0' && ch <= '9'){
+                digit = true;
+            }
+            else specialCharacter = true;
+        }
+
+        if(capitalLetter && smallLetter && digit && specialCharacter){
+            return true;
+        }
+        return false;
     }
 }
