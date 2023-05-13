d4f1ab63ec5b86ad27fb48ecb2f96977cb595b58
Gaurav Goswami <gauravg3070@gmail.com>
2023-05-13 18:49:48 +0530

    push 2

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,11 +1,12 @@
 package com.driver;
+import java.util.*;
 
 public class Email {
 
     private String emailId;
     private String password;
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -18,35 +19,45 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if (!oldPassword.equals(password))
+            return;
 
-        int flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0,flag5=0;
-        if(oldPassword == getPassword()){
-            if(newPassword.length() >= 8){
-                flag1 = 1;
-            }
-            for(int i = 0; i < newPassword.length(); i++) {
-                if ((newPassword.charAt(i) >= 'A' && newPassword.charAt(i) <= 'Z')) {
-                    flag2 = 1;
-                } else if ((newPassword.charAt(i) >= 'a' && newPassword.charAt(i) <= 'z')){
-                    flag3 = 1;
-                } else if ((newPassword.charAt(i) >= 48 && newPassword.charAt(i) <= 57)) {
-                    flag4 = 1;
-                }else{
-                    flag5 = 1;
-                }
-            }
+        if (newPassword.length() < 8)
+            return;
+
+        char[] arr = newPassword.toCharArray();
+        boolean isUpperCasePresent = false;
+        boolean islowercasePresent = false;
+        boolean isDigitPresent = false;
+        boolean isSpecialCharacterPresent = false;
+
+        int i = 0;
+
+        while (i < arr.length && (!isUpperCasePresent || !islowercasePresent || !isDigitPresent || !isSpecialCharacterPresent)) {
+
+            if (Character.isUpperCase(arr[i]))
+                isUpperCasePresent = true;
+            else if (Character.isLowerCase(arr[i]))
+                islowercasePresent = true;
+            else if (Character.isDigit(arr[i]))
+                isDigitPresent = true;
+            else
+                isSpecialCharacterPresent = true;
+
+            if (isUpperCasePresent && islowercasePresent && isDigitPresent && isSpecialCharacterPresent)
+                break;
+
+            i++;
         }
 
-        if((flag1+flag2+flag3+flag4+flag5)==5){
+        if (isUpperCasePresent && islowercasePresent && isDigitPresent && isSpecialCharacterPresent)
             password = newPassword;
-        }
-
     }
 }
